729c600245de5e79836413d16c80cc366e453867
olajo37 <ola.johansson@liu.se>
2023-08-31 09:21:13 +0200

    Maps crazyswarm and mocap to src in container. Maps the custom config map to the config map in the container (overwrites it).

---
+++ run.sh
@@ -0,0 +1,9 @@
+xhost +
+sudo docker run -it --env="DISPLAY" --net=host --rm --privileged\
+	-v /dev/bus/usb:/dev/bus/usb\
+	-v /dev/input:/dev/input\
+	-v /home/$USER/crazyswarm2/crazyswarm2:/root/ros2_ws/src/crazyswarm2\
+	-v /home/$USER/crazyswarm2/motion_capture_system:/root/ros2_ws/src/motion_capture_tracking\
+	-v /home/$USER/crazyswarm2/config:/root/ros2_ws/src/crazyswarm2/crazyflie/config\
+	-v /etc/udev:/etd/udev\
+	--name cs2_new cs2_new_img bash
