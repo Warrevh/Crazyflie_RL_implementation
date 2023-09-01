8b24146a54baa72dbdf01032a9a5f0e6be3063d7
olajo37 <ola.johansson@liu.se>
2023-09-01 14:05:56 +0200

    fixed wrong name

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ sudo docker run -it --env="DISPLAY" --net=host --rm --privileged\
 	-v /dev/bus/usb:/dev/bus/usb\
 	-v /dev/input:/dev/input\
 	-v /home/$USER/crazyswarm2/crazyswarm2:/root/ros2_ws/src/crazyswarm2\
-	-v /home/$USER/crazyswarm2/motion_capture_system:/root/ros2_ws/src/motion_capture_tracking\
+	-v /home/$USER/crazyswarm2/motion_capture_tracking:/root/ros2_ws/src/motion_capture_tracking\
 	-v /home/$USER/crazyswarm2/config:/root/ros2_ws/src/crazyswarm2/crazyflie/config\
+	-v /home/$USER/crazyswarm2/cf_nodes:/root/ros2_ws/src/cf_nodes\
 	-v /etc/udev:/etd/udev\
 	--name cs2_new cs2_new_img bash
