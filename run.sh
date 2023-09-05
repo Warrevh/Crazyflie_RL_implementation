6c3c2e459d8ae2b4752722b7283ea0fd805ece35
olajo37 <ola.johansson@liu.se>
2023-09-05 14:59:56 +0200

    corrected name of etc dir

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ sudo docker run -it --env="DISPLAY" --net=host --rm --privileged\
 	-v /home/$USER/crazyswarm2/motion_capture_tracking:/root/ros2_ws/src/motion_capture_tracking\
 	-v /home/$USER/crazyswarm2/config:/root/ros2_ws/src/crazyswarm2/crazyflie/config\
 	-v /home/$USER/crazyswarm2/cf_nodes:/root/ros2_ws/src/cf_nodes\
-	-v /etc/udev:/etd/udev\
+	-v /etc/udev:/etc/udev\
 	--name cs2_new cs2_new_img bash
