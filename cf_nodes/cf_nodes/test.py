9476b8dc7d746e79995d6930db2f1d984cae6b22
Warrevh <warrevh10@gmail.com>
2024-12-13 14:46:43 +0100

    added code for RL

---
+++ cf_nodes/cf_nodes/test.py
@@ -0,0 +1,15 @@
+"""Takeoff-hover-land for one CF. Useful to validate hardware config."""
+
+#from pycrazyswarm import Crazyswarm
+import sys
+from crazyflie_py import Crazyswarm
+import numpy as np
+
+def main():
+    print("test")
+    random_array = np.random.rand(3, 3)
+    print(random_array)
+
+if __name__ == "__main__":
+    main()
+
